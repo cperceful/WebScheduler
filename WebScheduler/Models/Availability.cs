3afcc6b481c369593ad07679c3086d0f1ff26906
cperceful <cpercef1@gmail.com>
2017-07-27 16:50:17 -0500

    Added classes for handling availability

---
+++ WebScheduler/Models/Availability.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebScheduler.Models
+{
+    public class Availability
+    {
+       public int ID { get; set; }
+
+        public Availability() { }
+
+        
+    }
+}
---
+++ WebScheduler/Models/AvailabilitySet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebScheduler.Models
+{
+    public class AvailabilitySet
+    {
+
+        public int ID { get; set; }
+
+        public string ApplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        
+
+        public IList<Availability> Availabilities { get; set; }
+    }
+}
