42da02e5db710e6d5b516c1dd663a9e9d0edfd77
cperceful <cpercef1@gmail.com>
2017-08-01 14:11:14 -0500

    ViewModel for RequestsOff

---
+++ WebScheduler/Models/StaffViewModels/AddEditRequestOffViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebScheduler.Models.StaffViewModels
+{
+    public class AddEditRequestOffViewModel
+    {
+        [Required]
+        public DateTime Date { get; set; }
+
+        public string Notes { get; set; }
+
+        [Required]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        public DateTime EndTime { get; set; }
+    }
+}
