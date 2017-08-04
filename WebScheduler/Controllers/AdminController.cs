9551d330af3a68891b0c612171949c636c3dde6d
cperceful <cpercef1@gmail.com>
2017-08-04 16:08:57 -0500

    View Availability for Admin

--- WebScheduler/Controllers/AdminController.cs
+++ WebScheduler/Controllers/AdminController.cs
@@ -4,18 +4,39 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using WebScheduler.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace WebScheduler.Controllers
-{
+{   
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
+        //UserManager object for accessing user store
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<ApplicationRole> roleManager;
+
+        public AdminController(UserManager<ApplicationUser> userManager, RoleManager<ApplicationRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Availability()
+        {
+            IList<ApplicationUser> users = userManager.Users.Include(x => x.Availabilities).Where(x => x.UserName != "Admin").ToList();
+            
+            return View(users);
         }
     }
 }
