9c5d38465be51cb007ffa2568be8e828a77cbd80
cperceful <cpercef1@gmail.com>
2017-09-12 15:53:10 -0500

    / path now only shows please log in if not logged in. Redirects to appropriate landing if logged in

--- WebScheduler/Controllers/HomeController.cs
+++ WebScheduler/Controllers/HomeController.cs
@@ -3,14 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using WebScheduler.Models;
 
 namespace WebScheduler.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public HomeController(UserManager<ApplicationUser> userManager)
         {
-            return View();
+            this.userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                return View();
+            }
+            else
+            {
+                ApplicationUser user = await userManager.FindByNameAsync(User.Identity.Name);
+                if (await userManager.IsInRoleAsync(user, "Admin"))
+                {
+                    return Redirect("/admin");
+                }
+                else
+                {
+                    return Redirect("/staff");
+                }
+            }
+            
         }
 
         public IActionResult About()
